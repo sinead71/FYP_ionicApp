48d9afc85f6776dadb90f891371fb5d1def19126
sinead.71 <cooneysinead71.com>
2017-02-01 20:07:09 +0000

    changing the tabs file names plus all links to it in other files.

--- myApp/src/pages/newMessage/newMessage.ts
+++ myApp/src/pages/newMessage/newMessage.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-newMessage',
+  templateUrl: 'newMessage.html'
 })
-export class ContactPage {
+export class newMessagePage {
 
   constructor(public navCtrl: NavController) {
 
